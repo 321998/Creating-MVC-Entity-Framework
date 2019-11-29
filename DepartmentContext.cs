0a8d70f717dfef4f50e094228210947794754340
321998 <51799777+321998@users.noreply.github.com>
2019-11-29 15:22:27 +0630

    Add files via upload

---
+++ DepartmentContext.cs
@@ -0,0 +1,20 @@
+﻿using DHH.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DHH.Context
+{
+    public class DepartmentContext: DbContext
+    {
+        public DepartmentContext() : base("EESSS") {
+        
+        }
+
+        public DbSet<Department> departments { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
